fd2b7fc4b9153eed64f3b69122e5a82fa3c1071d
mrvauxs <mrvauxs@gmail.com>
2026-06-07 22:17:06 +0200

    start working on random bs

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,22 +1,49 @@
 import moduleJSON from "moduleJSON";
 import { dev } from "$lib/utils";
+import type { TriggerApplication } from "trigger-engine/src/engine/application/application";
 
-function ready() {
+async function ready() {
 	if (dev) ui.notifications.info(`${moduleJSON.title} is ready!`);
+
+	console.log(await game.triggerEngine?.api.openBlueprintMenu("trigger-animations", "trigger-animations"));
+}
+
+async function registerApplication(register: typeof TriggerApplication.register) {
+	register(
+		moduleJSON.id,
+		moduleJSON.id,
+		{
+			mode: "setting",
+			builtins: {
+				entries: true,
+				convertors: true,
+				nodes: [
+					"execute-event", "await-confirm", "console-log", "create-message", "delete-item",
+					"execute-script", "update-item", "if-truthy", "is-combatant", "list-contains",
+					"extract-actor", "extract-item", "actors-match", "break-loop", "compare-numbers",
+					"filter-targets", "format-text", "resolve-formula", "texts-match", "split-boolean",
+					"split-number", "split-text", "current-combatant", "scene-targets", "user-value"
+				],
+			},
+		}
+	);
 }
 
 const hooks = {
-	ready: Hooks.on("ready", ready)
+	ready: Hooks.once("ready", ready),
+	"triggerEngine.registerApplication": Hooks.once("triggerEngine.registerApplication", registerApplication),
 };
 
 // Hot Module Replacement (HMR) used in development mode.
-// https://vite.dev/guide/api-hmr
-// Its simpler than you'd imagine!
 if (import.meta.hot) {
 	import.meta.hot.accept((newModule) => {
 		if (newModule) {
 			// Remove all old hooks
-			Object.entries(hooks).forEach(([k, h]) => Array.isArray(h) ? h.forEach((hook) => Hooks.off(k, hook)) : Hooks.off(k, h));
+			Object.entries(hooks).forEach(
+				([k, h]) => Array.isArray(h)
+					? h.forEach((hook) => Hooks.off(k, hook))
+					: Hooks.off(k, h)
+			);
 		}
 	})
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { API } from "./api";
 import "./module.css";
 import "./hooks"
+
+const { NodeEntry, NodeField, TriggerHook, TriggerNode } = globalThis.triggerEngine;
+
 // Expose the API on the module's global namespace
 globalThis.triggerAnimations = {
 	api: new API()
