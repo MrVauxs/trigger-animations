8855c1cd60e7b629aa1c54855f8ffe6d66b0eae4
mrvauxs <mrvauxs@gmail.com>
2026-06-13 08:11:28 +0200

    add end animation to api

--- src/api.ts
+++ src/api.ts
@@ -9,4 +9,10 @@ export class API {
 	runAnimation(data: StartNodeOptions) {
 		Hooks.callAll("trigger-animations.animate", data)
 	}
+	async endAnimation(opts: Parameters<typeof Sequencer.EffectManager.endEffects>[0]) {
+		return Sequencer.EffectManager.endEffects(opts)
+	}
+	async endAllAnimation(scene: string) {
+		return Sequencer.EffectManager.endAllEffects(scene)
+	}
 }
