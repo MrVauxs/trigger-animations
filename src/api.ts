cf6582f27efe694a641c43f985e2f0dee30f5b43
mrvauxs <mrvauxs@gmail.com>
2026-06-21 11:15:57 +0200

    a

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,11 @@ import { id } from "moduleJSON";
 import { TriggerEngine as T } from "trigger-engine/types";
 import { StartNodeOptions } from "./nodes";
 
-class API {
+export class API {
+	constructor() {
+		globalThis.triggerAnimations = { api: this };
+	}
+
 	openBlueprint(data?: T.TriggerDataInput, ...args: any[]) {
 		return game.triggerEngine?.api.openBlueprintMenu(id, id, data, ...args)
 	}
@@ -13,9 +17,29 @@ class API {
 		return Sequencer.EffectManager.endAllEffects(scene)
 	}
 	async runFromTrigger(data: StartNodeOptions): Promise<void> { };
+
+	_db: JournalEntry | undefined
+	get db() { return this._db }
+	set db(db) { this._db = db }
+
+	get setting(): Partial<T.TriggerApplicationOptions['setting']> {
+		return {
+			get: () => { },
+			set: () => { },
+			afterPrepared: () => { }
+		}
+	}
+
+	async createJournalDatabase() {
+		if (!game.user.isGM) return;
+		let database = game.journal.getName("Trigger Animations DB");
+		if (!database) {
+			database = await JournalEntry.create({
+				name: "Trigger Animations DB",
+				ownership: { default: CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER }
+			});
+		}
+		this._db = database;
+		return database;
+	}
 }
-
-export const api = new API();
-
-// Expose the API on the module's global namespace
-globalThis.triggerAnimations = { api };
--- src/register.ts
+++ src/register.ts
@@ -1,73 +1,65 @@
 import { id, title } from "moduleJSON";
 import { dev, devLog, log } from "$lib/utils";
 import type { TriggerEngine as T } from "trigger-engine/types";
-import { api } from "./api";
+import { API } from "./api";
 import * as tNodes from "./nodes/index"
 import * as tEntries from "./entries/index"
 import * as tHooks from "./hooks/index"
 
 type BuiltInKeys = { [k in T.TriggerApplicationCollection]: (typeof T.BuiltInApplication)[k][number][0][] };
 
-async function ready() {
-	if (dev) {
-		ui.notifications.info(`${title} is ready!`);
-		setTimeout(() => api.openBlueprint(), 1000);
-	}
-}
+Hooks.once("ready", async () => {
+	const api = new API();
+	await api.createJournalDatabase()
 
-const hooks = {
-	"ready": Hooks.once("ready", ready),
-	"triggerEngine.registerApplication": Hooks.on(
-		"triggerEngine.registerApplication",
-		(r: typeof T.TriggerApplication.register, builtInKeys: BuiltInKeys) => {
-			try {
-				const builtins: NonNullable<Parameters<typeof r>[2]>['builtins'] = {
-					// hooks: true,
-					entries: true,
-					convertors: true,
-					nodes: builtInKeys.nodes.filter(x => !x.includes("event")),
-				}
+	if (dev) setTimeout(() => api.openBlueprint(), 1000);
+});
 
-				devLog(
-					"Registering trigger-animations application",
-					{
-						nodes: Object.keys(tNodes),
-						hooks: Object.keys(tHooks),
-						entries: Object.keys(tEntries),
-						builtins
-					}
-				)
-
-				r(id, id, {
-					mode: "setting",
-					/*
-					// TODO: Make it edit a hidden Journal Document ala Sequencer
-					setting: {
-						get: () => {},
-						set: () => {}
-					},
-					*/
-					entries: Object.values(tEntries) as (typeof T.NodeEntry)[],
-					nodes: Object.values(tNodes) as (typeof T.TriggerNode)[],
-					hooks: Object.values(tHooks) as (typeof T.TriggerHook)[],
-					builtins,
-				});
-			} catch (e) {
-				ui.notifications.error(`Failed to register ${title} application.`, { permanent: true });
-				console.error(e);
+Hooks.on(
+	"triggerEngine.registerApplication",
+	(r: typeof T.TriggerApplication.register, builtInKeys: BuiltInKeys) => {
+		try {
+			const builtins: NonNullable<Parameters<typeof r>[2]>['builtins'] = {
+				// hooks: true,
+				entries: true,
+				convertors: true,
+				nodes: builtInKeys.nodes.filter(x => !x.includes("event")),
 			}
-		}
-	),
-	"triggerEngine.registerTriggers": Hooks.once("triggerEngine.registerTriggers", (registerTriggers) => {
-		log(`Registering triggers for trigger-engine`)
-		registerTriggers("trigger-engine", "pf2e-trigger", "modules/trigger-animations/static/pf2e-triggers.json");
 
-		for (const mod of game.modules) {
-			if (!mod.active) continue;
-			const flag = (mod?.flags?.['trigger-animations'] as { triggers: string })?.triggers;
-			if (!flag) continue;
-			log(`Registering triggers for ${mod.id}`, flag)
-			registerTriggers(id, id, flag);
+			devLog(
+				"Registering trigger-animations application",
+				{
+					nodes: Object.keys(tNodes),
+					hooks: Object.keys(tHooks),
+					entries: Object.keys(tEntries),
+					builtins
+				}
+			)
+
+			r(id, id, {
+				mode: "setting",
+				setting: globalThis.triggerAnimations.api.setting,
+				entries: Object.values(tEntries) as (typeof T.NodeEntry)[],
+				nodes: Object.values(tNodes) as (typeof T.TriggerNode)[],
+				hooks: Object.values(tHooks) as (typeof T.TriggerHook)[],
+				builtins,
+			});
+		} catch (e) {
+			ui.notifications.error(`Failed to register ${title} application.`, { permanent: true });
+			console.error(e);
 		}
-	})
-};
+	}
+);
+
+Hooks.once("triggerEngine.registerTriggers", (registerTriggers) => {
+	log(`Registering triggers for trigger-engine`)
+	registerTriggers("trigger-engine", "pf2e-trigger", "modules/trigger-animations/static/pf2e-triggers.json");
+
+	for (const mod of game.modules) {
+		if (!mod.active) continue;
+		const flag = (mod?.flags?.['trigger-animations'] as { triggers: string })?.triggers;
+		if (!flag) continue;
+		log(`Registering triggers for ${mod.id}`, flag)
+		registerTriggers(id, id, flag);
+	}
+});
