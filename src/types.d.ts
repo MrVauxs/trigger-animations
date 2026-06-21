903bced3310bd926cd94ef39a1a0ed96e69291b9
mrvauxs <mrvauxs@gmail.com>
2026-06-21 14:28:05 +0200

    Fix types #reonZ/trigger-engine/pull/15

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,8 @@
-import type { API } from "./api";
+import type { API as realAPI } from "./api";
 
 declare global {
 	namespace triggerAnimations {
-		// TODO: This is ugly to me, but Idle's types force my hand. Look into making this actually proper.
-		namespace api {
-			const db: API["db"];
-			const settingsMount: API["settingsMount"];
-			const openBlueprint: API["openBlueprint"];
-			const endAnimation: API["endAnimation"];
-			const endAllAnimation: API["endAllAnimation"];
-			const createJournalDatabase: API["createJournalDatabase"];
-		}
+		interface API extends realAPI { }
 	}
 }
 
